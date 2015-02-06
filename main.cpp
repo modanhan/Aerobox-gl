c7777dc1c0eff155050fe4df9830a587bbb49ddb
modan_han <hanhanhanronn@gmail.com>
2015-02-06 13:23:12 -0700

    added shading, literally just copied codes from openglprogramming website

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ using namespace std;
 aerobox::objfile o("box.obj");
 
 void init() {
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light_position[] = { 5.0, 5.0, 5.0, 0.0 };
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel (GL_SMOOTH);
 
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glEnable (GL_LIGHTING);
+	glEnable (GL_LIGHT0);
+	glEnable (GL_DEPTH_TEST);
 }
 
 void display() {
@@ -16,17 +28,16 @@ void display() {
 }
 
 void reshape(int w, int h) {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+	glMatrixMode (GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60, (GLfloat)w / (GLfloat)h, 0.1, 20.0);
-	glMatrixMode(GL_MODELVIEW);
+	gluPerspective(60, (GLfloat) w / (GLfloat) h, 0.1, 20.0);
+	glMatrixMode (GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(10.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	glutPostRedisplay();
 
 }
-
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
