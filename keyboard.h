becc3b048ec27727ea9f845d1e45afce89e37fd8
Modan Han <modanhan@live.com>
2015-02-10 15:13:23 -0700

    made keyboard buffer, see main for sample usage. known bug: when ctrl and alt are held down and the user punches the keyboard, some keys are detected to be always held down. bug created by the legendary JackieNim

---
+++ keyboard.h
@@ -0,0 +1,19 @@
+#include <vector>
+namespace aerobox {
+std::vector<bool> keydown(256, 0), keypressed(256, 0), keyreleased(256, 0);
+
+void keyboardPostRedisplayFunc() {
+	keypressed.assign(256, 0);
+	keyreleased.assign(256, 0);
+}
+
+void keyboardFunc(unsigned char key, int x, int y) {
+	keypressed[key] = 1;
+	keydown[key] = 1;
+}
+
+void keyboardUpFunc(unsigned char key, int x, int y) {
+	keyreleased[key] = 1;
+	keydown[key] = 0;
+}
+}
